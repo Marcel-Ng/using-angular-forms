9623c07e3edd86e8f7832071211fcaa9584feb1a
Marcel-Ng <uduma.matrix@gmail.com>
2020-04-27 14:46:43 +0100

    At this stage I have implemented some of the form aform functionality like using a post request from the data service for post etc

---
+++ src/app/data/data.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { UserSettings } from './user-settings';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  /**
+   * all the network request here should come from this place this is to avoid scattering things all around the
+   * the place and make things as encapsulated as possible
+   * @param http 
+   */
+
+  constructor(private http: HttpClient) { }
+
+  postUserSettingsForm(userSettings: UserSettings) : Observable <any>{
+    return this.http.post('http://localhost/living-stone/ps-form-demo.php', userSettings);
+    // return of(userSettings)
+  }
+}
